69d9b93a9ddb91e1b51a9b141e06418368deb8c9
叶俊钦 <yejunqin.qipeipu.com>
2016-09-09 18:04:46 +0800

    加入登陆页、注册页

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 module.exports = function(app){
   app.get('/', function(req, res){
-    res.render('index', {title: 'Express'})
+    res.render('index', {title: '主页'})
   })
-  app.get('/about', function(req, res){
-    res.send('this is about page')
+  app.get('/reg', function(req, res){
+    res.render('reg', {title: '注册'})
+  })
+  app.get('/login', function(req, res){
+    res.render('login', {title: '登陆'})
+  })
+  app.get('/post', function(req, res){
+    res.render('post', {title: '发表'})
+  })
+  app.get('/logout', function(req, res){
+
   })
 }
